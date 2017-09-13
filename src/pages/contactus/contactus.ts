054a7627e7dd73e1e41e8e084737b3909944a701
legendarybladerofmilkywaygalaxy <ee16b027@smail.iitm.ac.in>
2017-09-13 19:08:11 +0530

    contacts advance error

--- src/pages/contactus/contactus.ts
+++ src/pages/contactus/contactus.ts
@@ -7,18 +7,27 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactUsPage {
   titles: string[];
-  src : string[];
-  items: Array<{title: string}>;
+  names1: string[];
+  phone: string;
+  name: string;
+  data: string;
+  value: string;
+  items: Array<{title: string, names1: string}>;
   constructor(public navCtrl: NavController) {
     this.titles = ['Webops', 'Financs', 'Sponsorship and PR','Safety and Security','Media and VFX','Hospitality','Design and Ambience'];
+    this.names1 =['a','b','c','d','e','f','g'];
     this.items = [];
     for(let i = 0; i <7; i++) {
       this.items.push({
-        title: this.titles[i]
+        title: this.titles[i],
+        names1: this.names1[i]
       });
     }
   }
-  itemSelected(event, item) {
-    console.log(item.title);
+ itemSelected(name) {
+ if(name == "Webops"){
+ this.data = "hii"
+ }
   }
 }
+
