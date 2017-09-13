e09b2223ade0703a029e2dc5634060e11f2931f0
muqeeth <muqeeth101@gmail.com>
2017-09-13 21:34:30 +0530

    contact_basic4

--- src/pages/contactus/contactus.ts
+++ src/pages/contactus/contactus.ts
@@ -7,8 +7,9 @@ import { NavController ,AlertController} from 'ionic-angular';
 })
 export class ContactUsPage {
   titles: string[];
-  data : string;
-  phoneno:string;
+  data : string[];
+  phoneno:string[];
+  cord_data: Array<{name:string , phoneno:string}>;
   items: Array<{title: string}>;
   constructor(public navCtrl: NavController, public alertCtrl : AlertController) {
     this.titles = ['Webops', 'Finance', 'Sponsorship and PR','Safety and Security','Media and VFX','Hospitality','Design and Ambience'];
@@ -20,13 +21,42 @@ export class ContactUsPage {
     }
   }
   itemSelected(name) {
-      if(name == "Webops"){
-        this.data = "sriram";
-        this.phoneno= "9441463780"
-      }
-      if(name == "Finance"){
-        this.data = "Muqeeth";
-        this.phoneno = "8331963780"
+        if(name=="Webops"){
+        this.data = ["sriram","ashwin","muqeeth"];
+        this.phoneno= ["1234567809","86384783432","7435234341"]
+        this.cord_data =[];
+        for(let i = 0; i <3; i++) {
+          this.cord_data.push({
+            name: this.data[i],
+            phoneno:this.phoneno[i]
+          });
+        
       }
     }
+  
+  if(name== "Finance"){
+    this.data = ["sriram1","ashwin1","muqeeth1"];
+    this.phoneno= ["1234567809","86384783432","7435234341"]
+    this.cord_data =[];
+    for(let i = 0; i <3; i++) {
+      this.cord_data.push({
+        name: this.data[i],
+        phoneno:this.phoneno[i]
+      });
+    
+  }
+}
+if(name== "Sponsorship and PR"){
+  this.data = ["sriram2","ashwin2","muqeeth2"];
+  this.phoneno= ["1234567809","86384783432","7435234341"]
+  this.cord_data =[];
+  for(let i = 0; i <3; i++) {
+    this.cord_data.push({
+      name: this.data[i],
+      phoneno:this.phoneno[i]
+    });
+  
+  }
+}
+  }
   }
