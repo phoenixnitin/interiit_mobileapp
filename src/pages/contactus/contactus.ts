b566fa447001a1b5b50f7dc70b71c8fc4ec0e067
legendarybladerofmilkywaygalaxy <ee16b027@smail.iitm.ac.in>
2017-09-13 15:59:01 +0530

    contactbasics3

--- src/pages/contactus/contactus.ts
+++ src/pages/contactus/contactus.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contactus.html'
 })
 export class ContactUsPage {
-
+  titles: string[];
+  src : string[];
+  items: Array<{title: string}>;
   constructor(public navCtrl: NavController) {
-
+    this.titles = ['Webops', 'Financs', 'Sponsorship and PR','Safety and Security','Media and VFX','Hospitality','Design and Ambience'];
+    this.items = [];
+    for(let i = 0; i <7; i++) {
+      this.items.push({
+        title: this.titles[i]
+      });
+    }
   }
-
+  itemSelected(event, item) {
+    console.log(item.title);
+  }
 }
