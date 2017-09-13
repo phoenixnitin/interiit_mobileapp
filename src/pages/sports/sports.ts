e639cfac9b164a95addb77394dd85a17e827e0e0
muqeeth <muqeeth101@gmail.com>
2017-09-13 15:04:09 +0530

    sports_basic_2

--- src/pages/sports/sports.ts
+++ src/pages/sports/sports.ts
@@ -11,7 +11,7 @@ export class SportsPage {
   items: Array<{title: string, src: string}>;
   constructor(public navCtrl: NavController) {
     this.titles = ['Badminton', 'Basketball', 'Cricket','Football','Hockey','Squash','Swimming','Tennis','TableTennis','VolleyBall','WaterPolo','Weightlifting'];
-    this.src = ['./assets/img/badm.png','./assets/img/bask.png','./assets/img/cri.png','./assets/img/foot.png','./assets/img/hock.png','./assets/img/squa.png','./assets/img/swim.png','./assets/img/tenn.png','./assets/img/tt.png','./assets/img/volley.png','./assets/img/waterpolo.png','./assets/img/weight.png'];
+    this.src = ['../assets/img/badm.png','../assets/img/bask.png','../assets/img/cri.png','../assets/img/foot.png','../assets/img/hock.png','../assets/img/squa.png','../assets/img/swim.png','../assets/img/tenn.png','../assets/img/tt.png','../assets/img/volley.png','../assets/img/waterpolo.png','../assets/img/weight.png'];
     this.items = [];
     for(let i = 0; i <12; i++) {
       this.items.push({
