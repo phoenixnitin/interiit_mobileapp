b1e326147387eb89917545708e559243b623dffb
muqeeth <muqeeth101@gmail.com>
2017-10-08 22:07:11 +0530

    get from json

--- src/pages/contactus/contactus.ts
+++ src/pages/contactus/contactus.ts
@@ -1,62 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { NavController ,AlertController} from 'ionic-angular';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'page-contactus',
   templateUrl: 'contactus.html'
 })
-export class ContactUsPage {
-  titles: string[];
-  data : string[];
-  phoneno:string[];
-  cord_data: Array<{name:string , phoneno:string}>;
-  items: Array<{title: string}>;
-  constructor(public navCtrl: NavController, public alertCtrl : AlertController) {
-    this.titles = ['Webops', 'Finance', 'Sponsorship and PR','Safety and Security','Media and VFX','Hospitality','Design and Ambience'];
-    this.items = [];
-    for(let i = 0; i <7; i++) {
-      this.items.push({
-        title: this.titles[i]
-      });
-    }
-  }
-  itemSelected(name) {
-        if(name=="Webops"){
-        this.data = ["sriram","ashwin","muqeeth"];
-        this.phoneno= ["1234567809","86384783432","7435234341"]
-        this.cord_data =[];
-        for(let i = 0; i <3; i++) {
-          this.cord_data.push({
-            name: this.data[i],
-            phoneno:this.phoneno[i]
-          });
-
-      }
-    }
-
-  if(name== "Finance"){
-    this.data = ["sriram1","ashwin1","muqeeth1"];
-    this.phoneno= ["1234567809","86384783432","7435234341"]
-    this.cord_data =[];
-    for(let i = 0; i <3; i++) {
-      this.cord_data.push({
-        name: this.data[i],
-        phoneno:this.phoneno[i]
-      });
-
-  }
-}
-if(name== "Sponsorship and PR"){
-  this.data = ["sriram2","ashwin2","muqeeth2"];
-  this.phoneno= ["1234567809","86384783432","7435234341"]
-  this.cord_data =[];
-  for(let i = 0; i <3; i++) {
-    this.cord_data.push({
-      name: this.data[i],
-      phoneno:this.phoneno[i]
-    });
-
-  }
-}
-  }
+export class ContactUsPage implements OnInit{
+  cores: Array<object>;
+          constructor(private _http: Http){
+  
+          }
+          ngOnInit(){
+                  //-----------Main Code for Service---------//
+                  this._http.get('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1WjO92CQ-BLEqGuqZXzSo-wqTx6YE5PZrRpFtSHhxv-c&sheet=Core')
+                              .subscribe(res => {
+                                  this.cores = res.json().Core;
+                                  // console.log(this.cores);
+                              });
+              }
+  
   }
