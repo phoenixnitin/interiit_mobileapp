51e43620a752177b704c2ede6ef5b5c792b0aa5c
muqeeth <muqeeth101@gmail.com>
2017-12-06 11:13:50 +0530

    working as apk

--- src/pages/live/live.ts
+++ src/pages/live/live.ts
@@ -49,7 +49,7 @@ func();
     $(document).ready(function(){
     if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined'){
     var tag = document.createElement('script');
-    tag.src = "//www.youtube.com/iframe_api";
+    tag.src = "https://www.youtube.com/iframe_api";
     tag.id = "iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
