3cf218e544a8fe36004d10e7f4f449feb64eb82a
Shiv Tavker <tavkershiv@gmail.com>
2017-12-17 19:33:35 +0530

    Removed TypeError Sender ID is required parameter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -315,7 +315,7 @@ export class MyApp {
   pushsetup() {
     const options: PushOptions = {
        android: {
-           //senderID: '255556131441'
+           senderID: '255556131441'
        },
        ios: {
            alert: 'true',
